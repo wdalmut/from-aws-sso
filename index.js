a4de61adf887df6f78b6793d5862f154130d686a
Walter Dal Mut <walter.dalmut@gmail.com>
2020-11-15 13:16:40 +0000

    moved to assume-role instead token grab

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const fs = require('fs').promises
 const ini = require('ini')
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-const glob = util.promisify(require('glob'))
 const { program } = require('commander');
 const package = require('./package.json')
 
@@ -19,27 +18,23 @@ const sso_folder = path.join(homedir, ".aws/cli/cache")
 
 program
   .option('-p, --profile <profile>', 'update credentials profile with name', 'default')
+  .option('-d, --duration <duration>', 'session token duration in seconds', 3600)
 
 program.parse(process.argv);
 
-const get_latest_credentials = R.curry((sso_folder, account_id) => {
-  return glob(path.join(sso_folder, '*.json'))
-    .then(R.ifElse(R.length, R.identity, () => Promise.reject("Missing CLI credentials in .aws/cli/*.json")))
-    .then(R.compose(R.bind(Promise.all, Promise), R.map(fs.readFile)))
-    .then(R.map(JSON.parse))
-    .then(R.map(R.over(R.lensPath(['Credentials', 'Expiration']), R.compose(Date.parse, R.replace("UTC", "Z")))))
-    .then(R.sort(R.path(['Credentials', 'Expiration'])))
-    .then(R.compose(R.prop('Credentials'), R.head))
+const prepare_credentials = R.curry(identity => {
+  return Promise.resolve(R.prop('Credentials', identity))
+  
     .then(R.invertObj)
     .then(R.map(R.cond([
       [R.equals('AccessKeyId'), R.always('aws_access_key_id')],
       [R.equals('SecretAccessKey'), R.always('aws_secret_access_key')],
       [R.equals('SessionToken'), R.always('aws_session_token')],
+      [R.equals('Expiration'), R.always('expiration')],
       [R.T, R.F]
     ])))
-    .then(R.filter(R.identity))
     .then(R.invertObj)
-    .then(credentials => ({[account_id]: credentials}))
+    .then(credentials => ({[program.profile]: credentials}))
 })
 
 const create_aws_credentials_file = R.curry((aws_credentials, account_id) => {
@@ -59,10 +54,16 @@ const write_to_ini_file = R.curry((aws_credentials, new_credentials) => {
   return fs.writeFile(aws_credentials, updatedCredentials)
 })
 
+const get_user_from_current_identity = R.compose(R.last, R.split("/"))
+const get_role_arn_from_current_identity = R.compose(R.join("/"), R.slice(0, -1), R.insert(1, 'aws-reserved/sso.amazonaws.com/eu-central-1'), R.split("/"), R.replace("sts", "iam"), R.replace('assumed-role', 'role'))
+
 exec(`aws sts get-caller-identity --profile ${program.profile}`) // force CLI folder generation
-  .then(R.always(program.profile))
+  .then(R.compose(R.prop('Arn'), JSON.parse, R.prop('stdout')))
+  .then(identity_arn => [get_user_from_current_identity(identity_arn), get_role_arn_from_current_identity(identity_arn)])
+  .then(([user, role_arn]) => exec(`aws sts assume-role --duration-seconds ${program.duration} --role-arn ${role_arn} --role-session-name ${user} --profile ${program.profile}`))
+  .then(R.compose(JSON.parse, R.prop('stdout')))
   .then(create_aws_credentials_file(aws_credentials))
-  .then(get_latest_credentials(sso_folder))
+  .then(prepare_credentials)
   .then(update_credentials_in_ini_file(aws_credentials))
   .then(write_to_ini_file(aws_credentials))
   .then(() => console.log("Your profile is correctly updated!"))
